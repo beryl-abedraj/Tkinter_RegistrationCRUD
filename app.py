aa5ac444609018a069638a2dc8d239bec9517b2e
Beryl Abed Raj S <157201143+beryl-abedraj@users.noreply.github.com>
2024-03-13 22:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-import tkinter as tk
+import tkinter as tk                                                                                        # Importing Tkinter
 from tkinter import messagebox
-import mysql.connector
+import mysql.connector                                                                                      # Import MySQL Connector
 
-db = mysql.connector.connect(host="localhost", user="root", passwd="root", database="python_db")
+db = mysql.connector.connect(host="localhost", user="root", passwd="root", database="python_db")            
 cursor = db.cursor()
 
-def create_student():
+def create_student():                                                                                       # Creating CREATE CRUD Operator
     name = entry_name.get()
     email = entry_email.get()
     phone = entry_phone.get()
@@ -16,9 +16,9 @@ def create_student():
     salary = entry_salary.get()
     cursor.execute("INSERT INTO mister (name, email, phone, gender, department, year, company, salary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (name, email, phone, gender, department, year, company, salary))
     db.commit()
-    messagebox.showinfo("Success", "Student record created successfully.")
+    messagebox.showinfo("Success", "Student record created successfully.")                                  # Popup Message after UPLOAD on MySQL
 
-def reset_values():
+def reset_values():                                                                                         # Resetting Function to NULL after Uploaded 
     entry_name.delete(0, tk.END)
     entry_email.delete(0, tk.END)
     entry_phone.delete(0, tk.END)
@@ -28,15 +28,15 @@ def reset_values():
     entry_company.delete(0, tk.END)
     entry_salary.delete(0, tk.END)
 
-window = tk.Tk()
-window.title("Student Registration")
-window.configure(bg="#303030")
+window = tk.Tk()                                                                                            # Window Tkinter
+window.title("Student Registration")                                                                        # Title for Window
+window.configure(bg="#303030")                                                                              # Changing Background Color for Window
 
-label_name = tk.Label(window, text="Name :")
+
+label_name = tk.Label(window, text="Name :")                                                                # Creating Label to Enter Data
 label_name.grid(row=0, column=0, padx=50, pady=25)
 entry_name = tk.Entry(window)
 entry_name.grid(row=0, column=1, padx=10, pady=5)
-
 
 label_email = tk.Label(window, text="Email :")
 label_email.grid(row=1, column=0, padx=50, pady=25)
@@ -53,9 +53,9 @@ label_gender.grid(row=3, column=0, padx=50, pady=25)
 entry_gender = tk.Entry(window)
 entry_gender.grid(row=3, column=1, padx=10, pady=5)
 
-departments = ["CSE", "MECH", "ECE", "EEE", "AI & DS", "AUTO", "CIVIL", "EIE", "IT"]
+departments = ["CSE", "MECH", "ECE", "EEE", "AI & DS", "AUTO", "CIVIL", "EIE", "IT"]                      # Array to hold Dropdown Menu Values
 
-selected_department = tk.StringVar(window)
+selected_department = tk.StringVar(window)                                                                # DropDown Menu for Department
 selected_department.set("Select")
 
 label_department = tk.Label(window, text="Department :")
@@ -78,10 +78,10 @@ label_salary.grid(row=7, column=0, padx=50, pady=25)
 entry_salary = tk.Entry(window)
 entry_salary.grid(row=7, column=1, padx=10, pady=5)
 
-button_create = tk.Button(window, text="UPLOAD", command=create_student, bg="#0077b6", fg="#ffffff")
+button_create = tk.Button(window, text="UPLOAD", command=create_student, bg="#0077b6", fg="#ffffff")     # Creating Button to UPLOAD into MySQL
 button_create.grid(row=8, column=0, padx=10, pady=5)
 
-button_reset = tk.Button(window, text="RESET", command=reset_values, bg="#0077b6", fg="#ffffff")
+button_reset = tk.Button(window, text="RESET", command=reset_values, bg="#0077b6", fg="#ffffff")         # Creating Button to Reset residue values 
 button_reset.grid(row=8, column=1, padx=10, pady=5)
 
 window.mainloop()
